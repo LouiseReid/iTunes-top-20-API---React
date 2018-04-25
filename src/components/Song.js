3f728f09913e780bbeb89976d585292efb3b86cc
Louise Reid <l_reid@hotmail.co.uk>
2018-04-25 10:30:45 +0100

    refactoring

--- src/components/Song.js
+++ src/components/Song.js
@@ -1,14 +1,23 @@
-import React from 'react'
+import React from 'react';
 
-const Song = ({song}) => {
-  if(!song){
-    return null
-  }
-  return(
-    <h3>{song.name}</h3>
+const Song = (songs) => {
+  if(!songs)return null;
+
+  return (
+    <div className="container">
+      <div id="song-details-container">
+        <h1 className="position">{songs.position}</h1>
+        <img src={songs.image} alt={songs.title}/>
+        <div className="songs-details">
+          <h2>{songs.title}</h2>
+          <audio controls>
+            <source src={songs.audio} type="audio/mp4" />
+          </audio>
+          <h5 className="label">{songs.label}</h5>
+        </div>
+      </div>
+    </div>
   )
 }
 
-
-
-export default Song
+export default Song;
--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,38 +1,27 @@
-import React from 'react'
+import React from 'react';
+import Song from './Song'
 
-class SongList extends React.Component {
-  render(){
+const SongList = (props) => {
+  if(!props.songs)return;
 
-    if(!this.props.songs) return null
-    const songslist = this.props.songs.map((song, index) => {
-      return(
-        <div key={index} className="container">
-          <div id="song-details-container">
-            <h1 className="position">{index +1}</h1>
-              <img src={song["im:image"][2].label} alt={song["im:artist"].label}/>
-            <div className="song-details">
-              <h2>{song.title.label}</h2>
-              <h3>{song["im:artist"].label}</h3>
-              <audio controls>
-                <source src={song.link[1].attributes.href} type="audio/mp4" />
-              </audio>
-              <h5>{song.rights.label}</h5>
-            </div>
-          </div>
-        </div>
-
-      )
-    })
-
-    return(
-      <div>
-        {songslist}
-      </div>
+  const songs = props.songs.map((song, index)=>{
+    return (
+      <Song
+        key={index}
+        position={index + 1}
+        image={song["im:image"][2].label}
+        title={song.title.label}
+        audio={song.link[1].attributes.href}
+        label={song.rights.label}
+      />
     )
-  }
+  })
+
+  return (
+    <React.Fragment>
+      {songs}
+    </React.Fragment>
+  )
 }
 
-
-
-
-export default SongList
+export default SongList;
